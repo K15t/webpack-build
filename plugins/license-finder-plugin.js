5e1bbf150250869693bca7faf18fcacab6a74163
Sven Walter <sven@k15t.com>
2016-09-26 20:48:16 +0200

    Reorganized dependencies and enforce folder creation before the summary license files will be saved

--- plugins/license-finder-plugin.js
+++ plugins/license-finder-plugin.js
@@ -1,96 +1,113 @@
 'use strict';
 
 var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
 var nlf = require('nlf');
 var formatter = require('nlf/lib/formatters/standard');
 var RawSource = require('webpack-core/lib/RawSource');
 
 function LicenseFinderPlugin(config) {
-  this.base = config.base;
-  this.licenses = getLicenses(config.licenseConfig);
-  this.outputFile = config.outputFile;
-  this.notAllowedLicenses = config.notAllowedLicenses || [];
+    this.base = config.base;
+    this.licenses = getLicenses(config.licenseConfig);
+    this.outputFile = config.outputFile;
+    this.notAllowedLicenses = config.notAllowedLicenses || [];
 }
 
 function getLicenses(licenseConfig) {
-  var file, licenses;
+    var file, licenses;
 
-  try {
-    file = fs.readFileSync(licenseConfig, 'utf8');
-    licenses = JSON.parse(file);
+    try {
+        file = fs.readFileSync(licenseConfig, 'utf8');
+        licenses = JSON.parse(file);
 
-  } catch (e) {
-    console.log('Warning, couldn\'t read license config: ' + licenseConfig + '\n');
-  }
+    } catch (e) {
+        console.log('Warning, couldn\'t read license config: ' + licenseConfig + '\n');
+    }
 
-  return licenses || {};
+    return licenses || {};
 }
 
 LicenseFinderPlugin.prototype = {
 
-  apply: function(compiler) {
-    var that = this;
+    apply: function(compiler) {
+        var that = this;
 
-    compiler.plugin('emit', function(compilation, callback) {
-      nlf.find({
-        directory: that.base,
-        production: true
-      }, function(err, dependencies) {
+        compiler.plugin('emit', function(compilation, callback) {
+            nlf.find({
+                directory: that.base,
+                production: true
+            }, function(err, dependencies) {
 
-        if (err) {
-          throw new Error('Parsing licenses failed: ' + JSON.stringify(err));
+                if (err) {
+                    throw new Error('Parsing licenses failed: ' + JSON.stringify(err));
 
-        } else {
-          dependencies = dependencies.filter(function(dependency) {
-            return dependency.id !== (process.env.npm_package_name + "@" + process.env.npm_package_version);
-          });
+                } else {
+                    dependencies = dependencies.filter(function(dependency) {
+                        return dependency.id !== (process.env.npm_package_name + "@" + process.env.npm_package_version);
+                    });
 
-          var unlicensedDepedencies = that.getUnlicensed(dependencies);
+                    var unlicensedDepedencies = that.getUnlicensed(dependencies);
 
-          if (unlicensedDepedencies.length > 0) {
-            throw new Error(that.formatDependencyError(unlicensedDepedencies));
+                    if (unlicensedDepedencies.length > 0) {
+                        throw new Error(that.formatDependencyError(unlicensedDepedencies));
 
-          } else {
-            formatter.render(dependencies, {}, function(err, output) {
+                    } else {
+                        formatter.render(dependencies, {}, function(err, output) {
 
-              if (err) {
-                throw new Error('Formatting license file failed: ' + JSON.stringify(err));
-              }
-              console.log('License summary file: ' + that.outputFile);
-              fs.writeFile(that.outputFile, output, function(err) {
-                if (err) return console.log(err);
-              });
-              callback();
+                            if (err) {
+                                throw new Error('Formatting license file failed: ' + JSON.stringify(err));
+                            }
+                            console.log('License summary file: ' + that.outputFile);
+
+                            var outputFolder = path.dirname(that.outputFile);
+                            if (!fs.existsSync(outputFolder)) {
+
+                                mkdirp(outputFolder, function(err) {
+                                    if (err) {
+                                        console.error(err);
+                                    } else {
+                                        fs.writeFile(that.outputFile, output, function(err) {
+                                            if (err) return console.log(err);
+                                        });
+                                    }
+                                });
+                            } else {
+                                fs.writeFile(that.outputFile, output, function(err) {
+                                    if (err) return console.log(err);
+                                });
+                            }
+                            callback();
+                        });
+                    }
+                }
             });
-          }
-        }
-      });
-    });
-  },
+        });
+    },
 
-  getLicense: function(dependency) {
-    return this.licenses[dependency.id] || dependency.licenseSources.package.summary()[0] || 'none';
-  },
+    getLicense: function(dependency) {
+        return this.licenses[dependency.id] || dependency.licenseSources.package.summary()[0] || 'none';
+    },
 
-  getUnlicensed: function getUnlicensed(dependencies) {
-    return dependencies.filter(function(dependency) {
-      var license = this.getLicense(dependency);
-      console.log("Check license for " + dependency.id + " ... is licensed under " + license);
-      return license == 'none' || this.notAllowedLicenses.indexOf(license) !== -1;
-    }.bind(this));
-  },
+    getUnlicensed: function getUnlicensed(dependencies) {
+        return dependencies.filter(function(dependency) {
+            var license = this.getLicense(dependency);
+            console.log("Check license for " + dependency.id + " ... is licensed under " + license);
+            return license == 'none' || this.notAllowedLicenses.indexOf(license) !== -1;
+        }.bind(this));
+    },
 
-  formatDependencyError: function(dependencies) {
-    var dependenciesList = dependencies.map(function(dependency) {
-      return ' ' + dependency.id + ': ' + this.getLicense(dependency);
-    }.bind(this)).join('\n');
+    formatDependencyError: function(dependencies) {
+        var dependenciesList = dependencies.map(function(dependency) {
+            return ' ' + dependency.id + ': ' + this.getLicense(dependency);
+        }.bind(this)).join('\n');
 
-    return (
-        'License check failed. Not allowed license used! \n' +
-        'All not allowed license: ' + this.notAllowedLicenses.join(',') + '\n' +
-        'The following libraries are critical: \n' + dependenciesList
-    );
-  }
+        return (
+            'License check failed. Not allowed license used! \n' +
+            'All not allowed license: ' + this.notAllowedLicenses.join(',') + '\n' +
+            'The following libraries are critical: \n' + dependenciesList
+        );
+    }
 
 };
 
